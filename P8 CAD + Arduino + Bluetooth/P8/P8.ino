1c3422d392c15aa167b5adb4787732641ebfa3e5
broken_erk <enriquebroly@hotmail.com>
2018-11-26 12:35:47 -0600

    codigo p8 funcionando al cien

--- P8 CAD + Arduino + Bluetooth/P8/P8.ino
+++ P8 CAD + Arduino + Bluetooth/P8/P8.ino
@@ -1,8 +1,8 @@
 double bits[8], decimal;
-byte temp, grados;
+byte temp, angulo;
 const int a = 22, b = 24, c = 26, d = 28, e = 30, f = 32, g = 34, h = 36, salida = 9;
 const double resolucion = 5.00/255.00;
-int estado;
+int val = '0', estado = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,28 +32,29 @@ void loop() {
 
   decimal = bits[0] + bits[1]*2 + bits[2]*4 + bits[3]*8 + bits[4]*16 + bits[5]*32 + bits[6]*64 + bits[7]*128;
   temp = resolucion*decimal*10;
-  byte val = map(temp,0.00,50.00,0,255);
-  grados = resolucion*decimal*57;
+  byte grados = map(temp,0.00,50.00,0,255);
+  angulo = resolucion*decimal*57;
 
-  //estado = Serial3.read();
-  if(Serial.available() > 0)
-  {
-    estado = Serial.read();
-    //estado = Serial3.read();
-    //Serial.println(estado);
-  }
+   val = Serial3.read();
+   if(val == '1')
+   {
+     estado = 1;
+   }
+   else if(val == '0')
+   {
+     estado = 0;
+   }
 
-  if(estado == '1')
-  {
-    digitalWrite(salida, LOW);
-    Serial.println("OFF");
-    //Serial3.write(grados);
-  }
-  else
+  if(estado == 0)
   {
     digitalWrite(salida, HIGH); 
-    Serial.println("ON");
-    //Serial3.write(val);
+    Serial.println("Canal 0");
+    Serial3.write(grados);
   }
-  delay(500);
+  else if(estado == 1)
+  {
+    digitalWrite(salida, LOW);
+    Serial.println("Canal 1");
+    Serial3.write(angulo);
+  }
 }
